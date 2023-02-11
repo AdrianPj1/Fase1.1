53d0e6eb9cce27d43073044e88b42fe3296a2df0
AdrianPj1 <AdrianPj1@users.noreply.github.com>
2023-02-10 19:27:19 -0600

    Carga inicial

---
+++ Carpeta 4/practica1/New Unity Project/Assets/SCRIPS/Movimiento.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public Vector2 direccion;
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        direccion = new Vector2( Input.GetAxis("Horizontal"), Input.GetAxis("Vertica
+    }
+}
